28fb87de2f2a3ebef562169f7b9a61beb76ffa5a
Martin Deinhofer <deinhofer.m@gmail.com>
2022-08-29 12:07:21 +0200

    added long example text

--- examples/remote_control/SpeechController_gTTS.py
+++ examples/remote_control/SpeechController_gTTS.py
@@ -22,14 +22,14 @@ class SpeechController:
 
     def synthandspeak(self, text, lang="en"):
         print("in synthandspeak")
-        gTTS(text=text, lang=lang).write_to_fp(voice := NamedTemporaryFile())
-        playsound(voice.name, False)
+        gTTS(text=text, lang=lang, slow=False).write_to_fp(voice := NamedTemporaryFile())
+        playsound(voice.name, True)
         voice.close()
 
     def kill_proc(self):
         # nothing to do
         pass
 
-#speech_controller=SpeechController()
-#speech_controller.say("This is an English text")
+speech_controller=SpeechController()
+speech_controller.say("I am very hungry, what about lunch? This is a very long text. I would like to eat something fancy!!")
 #speech_controller.say("Das ist ein deutscher Text","de")
