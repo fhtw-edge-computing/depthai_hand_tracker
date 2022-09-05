abd14117ed0f899526b00ad4dac5a81d73489c85
Martin Deinhofer <deinhofer.m@gmail.com>
2022-09-05 18:13:40 +0200

    do playback with byte buffer and pygame library

--- examples/remote_control/SpeechController_gTTS.py
+++ examples/remote_control/SpeechController_gTTS.py
@@ -1,35 +1,34 @@
-import asyncio
-
-from gtts import gTTS
 from io import BytesIO
-from playsound import playsound
-from io import BytesIO
-
 from gtts import gTTS
-from playsound import playsound
-from tempfile import NamedTemporaryFile
-import concurrent.futures
+from pygame import mixer
+import time
+
+mixer.init() #Initialzing pyamge mixer
 
 class SpeechController:
     """ This class handles the speech output to give an auditive feedback to the user's input. """
 
     def say(self, text, lang="en"):
-#        mp3_fp = BytesIO()
-#        tts = gTTS('hello', lang='en')
-#        tts.write_to_fp(mp3_fp)
-#        playsound()
-        self.synthandspeak(text,lang)
+        mp3_fp = BytesIO()
+        tts = gTTS(text=text, lang=lang, slow=False)
+        tts.write_to_fp(mp3_fp)
+        mp3_fp.seek(0)
 
-    def synthandspeak(self, text, lang="en"):
-        print("in synthandspeak")
-        gTTS(text=text, lang=lang, slow=False).write_to_fp(voice := NamedTemporaryFile())
-        playsound(voice.name, True)
-        voice.close()
+        mixer.music.load(mp3_fp,"mp3")  # Loading Music File
+        mixer.music.play()  # Playing Music with Pygame
 
     def kill_proc(self):
         # nothing to do
+        mixer.music.stop()
         pass
 
-speech_controller=SpeechController()
-speech_controller.say("I am very hungry, what about lunch? This is a very long text. I would like to eat something fancy!!")
-#speech_controller.say("Das ist ein deutscher Text","de")
+if __name__ == '__main__':
+    speech_controller = SpeechController()
+    speech_controller.say(
+        "I am very hungry, what about lunch? This is a very long text. I would like to eat something fancy!!")
+    time.sleep(3)
+    speech_controller.say("Das ist ein deutscher Text", "de")
+
+    time.sleep(2)
+    print("After sleep, stopping sound")
+    speech_controller.kill_proc()
